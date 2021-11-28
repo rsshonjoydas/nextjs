5e7fc011280a82fee4d4af55beb8ff5761b02bac
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-28 16:07:54 +0600

    getStaticPaths fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/dist/client/router"
+
 const Post = ({ post }) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <h2>{post.id} {post.title}</h2>
@@ -34,7 +42,7 @@ export async function getStaticPaths() {
       },
     ],
     // paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
@@ -42,6 +50,13 @@ export async function getStaticProps(context) {
   const { params } = context
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
   const data = await response.json()
+
+  if (!data.id) {
+    return {
+      notFound: true
+    }
+  }
+  console.log(`Generating page for /posts/${params.postId}`);
 
   return {
     props: {
