9dd4b9c6b87f4c7c9daa3763df4c0903571a28f1
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-28 09:41:38 +0600

    SSG with Dynamic Parameters

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <h1>Next JS pre-rendering</h1>
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href="/users">
+        <a>Users</a>
+      </Link>
+      <Link href="/posts">Posts</Link>
+    </>
   )
 }
