2dcd766922a59a894cec225ce60d2d33585a8e37
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-28 16:15:06 +0600

    getStaticPaths fallback blocking

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,12 +1,4 @@
-import { useRouter } from "next/dist/client/router"
-
 const Post = ({ post }) => {
-  const router = useRouter()
-
-  if (router.isFallback) {
-    return <h1>Loading...</h1>
-  }
-
   return (
     <>
       <h2>{post.id} {post.title}</h2>
@@ -42,7 +34,7 @@ export async function getStaticPaths() {
       },
     ],
     // paths,
-    fallback: true,
+    fallback: 'blocking',
   }
 }
 
