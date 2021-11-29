3afd0226b91634c72f5a2e44f0e96c15c7668059
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-29 08:40:36 +0600

    App Layout

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="layout-footer">
+      Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="layout-header">
+      Header
+    </div>
+  )
+}
+
+export default Header
