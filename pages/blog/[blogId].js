610c67b3924a1ba1db8114527ae0b0befd74c2d9
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-29 10:09:36 +0600

    Head component for spacific file

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+const Blog = ({ title, description }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+      </Head>
+      <h1 className='content'>
+        Article
+      </h1>
+    </>
+  )
+}
+
+export default Blog
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: 'Article Title',
+      description: 'Article description'
+    }
+  }
+}
