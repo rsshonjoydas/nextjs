87c2ac697a7dbae5a43b54356175322728f9cf00
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-29 08:12:10 +0600

    SASS Support

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import styles from '../styles/about.module.css'
+import scssStyles from '../styles/About.module.scss'
 
 const About = () => {
   return (
     <>
       <h2>About Page</h2>
       <h2 className={styles.highlight}>Module css</h2>
+      <h2 className={scssStyles.highlightScss}>SCSS Styles</h2>
     </>
     
   )
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,12 @@
 import styles from '../styles/contact.module.css'
+import scssStyles from '../styles/Contact.module.scss'
 
 const Contact = () => {
   return (
     <>
       <h2>Contact Page</h2>
       <h2 className={styles.highlight}>module css</h2>
+      <h2 className={scssStyles.highlightScss}>SCSS Styles</h2>
     </>
   )
 }
