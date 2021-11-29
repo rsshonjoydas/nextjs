3fb0dbd939393c3d93286cdc8ba1299acaf2bd5a
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-29 08:00:04 +0600

    Component Level Styles

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,12 @@
+import styles from '../styles/about.module.css'
+
 const About = () => {
   return (
-    <h2>About Page</h2>
+    <>
+      <h2>About Page</h2>
+      <h2 className={styles.highlight}>Module css</h2>
+    </>
+    
   )
 }
 
--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,11 @@
+import styles from '../styles/contact.module.css'
+
 const Contact = () => {
   return (
-    <h2>Contact Page</h2>
+    <>
+      <h2>Contact Page</h2>
+      <h2 className={styles.highlight}>module css</h2>
+    </>
   )
 }
 
