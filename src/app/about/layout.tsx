03645dbf7884b2c6a4ff08899f95fd5682d31847
”rsshonjoydas” <rsshonjoydas@email.com>
2023-04-12 19:32:21 +0600

    metadata api

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from 'next';
 import type { FC, ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Redolence | About',
+  description: 'MERN Stack Web Developer',
+  keywords:
+    'Redolence, web developer, HTML, CSS, SASS, Tailwindcss, JavaScript, Typescript, ReactJS, NextJS, NodeJS, ExpressJS, MondoDB, Redis, Firebase and Docker',
+};
 
 interface AboutLayoutProps {
   children: ReactNode;
