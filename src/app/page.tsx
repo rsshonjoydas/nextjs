55eb66eb925f37651ae1746445a4e5a7bd1070fa
”rsshonjoydas” <rsshonjoydas@email.com>
2023-04-12 22:50:53 +0600

    fetching course data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Courses from '@/components/Courses';
 import { FC } from 'react';
 
 interface HomePageProps {}
@@ -7,17 +7,7 @@ const HomePage: FC<HomePageProps> = ({}) => {
   return (
     <>
       <h1>Welcome To Redolence</h1>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-        <li>
-          <Link href='/about/team'>Team</Link>
-        </li>
-      </ul>
+      <Courses />
     </>
   );
 };
