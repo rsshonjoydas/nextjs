96e3f59792d35586df6c987d5d6f13a6c0c8a023
”rsshonjoydas” <rsshonjoydas@email.com>
2023-04-12 22:19:50 +0600

    suspense boundaries

--- src/app/code/repos/[name]/page.tsx
+++ src/app/code/repos/[name]/page.tsx
@@ -1,7 +1,7 @@
 import Repo from '@/components/Repo';
 import RepoDirs from '@/components/RepoDirs';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
 
 interface RepoPageProps {}
 
@@ -11,8 +11,12 @@ const RepoPage: FC<RepoPageProps> = ({ params: { name } }: any) => {
       <Link href='/code/repos' className='btn btn-back'>
         Back To Repositories
       </Link>
-      <Repo name={name} />
-      <RepoDirs name={name} />
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   );
 };
