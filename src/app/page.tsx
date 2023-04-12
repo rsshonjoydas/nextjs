e7ad3dc311fded45ae210fb6c5d8e102f72cf0da
”rsshonjoydas” <rsshonjoydas@email.com>
2023-04-12 19:09:45 +0600

    routing system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import Link from 'next/link';
 import { FC } from 'react';
 
 interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = ({}) => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <h1>Welcome To Redolence</h1>
+      <ul>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+        <li>
+          <Link href='/about/team'>Team</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default HomePage;
