95210fcf396201df7b7350ebafabbe1801ca7bba
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 19:05:02 +0600

    conditional route

---
+++ src/app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,7 @@
+import { Card } from '@/components/card';
+
+const Login = () => {
+  return <Card>Pleas login to continue</Card>;
+};
+
+export default Login;
--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -3,14 +3,18 @@ const ComplexDashboardLayout = ({
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) => {
-  return (
-    <>
+  const isLoggedIn = true;
+
+  return isLoggedIn ? (
+    <div>
       <div>{children}</div>
       <div className='flex'>
         <div className='flex flex-col'>
@@ -19,7 +23,9 @@ const ComplexDashboardLayout = ({
         </div>
         <div className='flex flex-1'>{notifications}</div>
       </div>
-    </>
+    </div>
+  ) : (
+    login
   );
 };
 
