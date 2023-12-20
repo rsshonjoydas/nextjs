07d9357e6b861389e4e902d8f942b1f767df76e1
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 17:09:07 +0600

    title metadata

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+};
+
 const FirstBlog = () => {
   return <div>FirstBlog</div>;
 };
--- src/app/blog/second/page.tsx
+++ src/app/blog/second/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Blog',
+  },
+};
+
 const SecondBlog = () => {
   return <div>SecondBlog</div>;
 };
