01653112d94558463780f811bf08757d05df30ad
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 08:15:33 +0600

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,14 @@
+import { notFound } from 'next/navigation';
+
 const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
+
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
