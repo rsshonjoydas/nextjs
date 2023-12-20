b1363f7faee5a6e44e018b5e9663182668c4af08
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 18:06:12 +0600

    recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,17 @@
 'use client';
 
-const Error = ({ error }: { error: Error }) => {
-  return <div>{error.message}</div>;
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  return (
+    <div>
+      {error.message} <button onClick={reset}>Try again</button>
+    </div>
+  );
 };
 
-export default Error;
+export default ErrorBoundary;
