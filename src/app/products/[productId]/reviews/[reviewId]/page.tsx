befef1fc52c53076f8755b2ffd3136fd095bfb10
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 07:57:31 +0600

    nested dynamic routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+const ReviewDetail = ({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) => {
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
+};
+
+export default ReviewDetail;
