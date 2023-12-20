c7780e61a3631fb28aecb2445ad32bc73e3ae6c7
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 17:21:00 +0600

    link component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <Link href='/blog'>Blog</Link>
+      <Link href='/products'>Products</Link>
+      <Link href='/docs'>Docs</Link>
+    </>
+  );
 };
 
 export default Home;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,26 @@
+import Link from 'next/link';
+
 const ProductList = () => {
+  const productId = 37;
+
   return (
     <div>
+      <Link href='/'>Home</Link>
       <h1>Product List</h1>
-      <h3>Product 1</h3>
-      <h3>Product 2</h3>
-      <h3>Product 3</h3>
+      <h3>
+        <Link href='/products/1'>Product 1</Link>
+      </h3>
+      <h3>
+        <Link href='/products/2'>Product 2</Link>
+      </h3>
+      <h3>
+        <Link href='/products/3' replace>
+          Product 3
+        </Link>
+      </h3>
+      <h3>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h3>
     </div>
   );
 };
