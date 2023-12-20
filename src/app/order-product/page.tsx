b6f88a93a678be81c4e5820798f6e0dc6816d65c
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 17:37:39 +0600

    navigating programmatically

---
+++ src/app/order-product/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+const OrderProduct = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing your order');
+    router.push('/');
+  };
+
+  return (
+    <>
+      <h1>Order Product</h1>
+      <button type='submit' onClick={handleClick}>
+        Place order
+      </button>
+    </>
+  );
+};
+
+export default OrderProduct;
