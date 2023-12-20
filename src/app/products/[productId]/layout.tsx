d44a17f48ffe0d9b7a81e73b07d3891ab1e70684
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-20 18:14:40 +0600

    handling errors in layout

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 const ProductIdLayout = ({ children }: { children: React.ReactNode }) => {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error('Error loading review');
+  }
+
   return (
     <div>
       {children}
